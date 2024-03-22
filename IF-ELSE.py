b0ac1714ddf1107c0c5ebecd7914d17bb78d76b7
Akshita Tiwari <85664297+tiwari1@users.noreply.github.com>
2024-03-22 08:46:54 +0530

    Add files via upload

---
+++ IF-ELSE.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Conditional statements
+1. if - else
+2. elif --> for multiple conditions
+3. nested if
+
+
+# In[ ]:
+
+
+# if - else
+# basic syntax
+if condition:
+    print()
+else:
+    print()
+
+
+# In[ ]:
+
+
+# user input
+1. string
+2. int
+3. float
+
+
+# In[1]:
+
+
+# str
+x=input('enter jrbgk-->')
+print(x)
+print(type(x))
+
+
+# In[2]:
+
+
+# int
+x=int(input('enter jrbgk-->'))
+print(x)
+print(type(x))
+
+
+# In[3]:
+
+
+# float
+x=float(input('enter jrbgk-->'))
+print(x)
+print(type(x))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
